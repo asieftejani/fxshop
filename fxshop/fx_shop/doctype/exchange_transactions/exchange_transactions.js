a11224537147ba63fce530797eb5adf90dcca866
Asief Tejani <asief.tejani@gmail.com>
2026-09-15 08:56:41 +0000

    remove fx_shop and changed readme

--- fxshop/fx_shop/doctype/exchange_transactions/exchange_transactions.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Asief Tejani and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Exchange Transactions", {
-// 	refresh(frm) {
-
-// 	},
-// });
